c78882ec658821980047c987a71d1f55d19c78df
Erik Petersen <Erikpete7@gmail.com>
2026-07-06 16:42:19 -0700

    Complete Lab 2 assignment

--- Lab_2/main.cpp
+++ Lab_2/main.cpp
@@ -6,9 +6,31 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+int main() {
+    int age;
+    double height;
+
+    cout << " Ride Eligibility Checker" << endl;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Enter your height (in inches): ";
+    cin >> height;
+
+    if ((12 <= age) && (age <= 75) && ( 40 <=height ) && (height<=80))  {
+        cout << "Congratulations! You are eligible to ride the roller coaster!" << endl;
+    }
+    else if (((75 < age) || (age < 12)) && (( 80 <height ) || (height<40))) {
+        cout << "Sorry, you did not meet the height requirement or the age requirment." << endl;
+    }
+    else if  ((75 < age) || (age < 12)) {
+        cout << "Sorry, you did not meet the age requirement, but are tall enough to ride." << endl;
+    }
+    else {
+        cout << "Sorry, you do not meet the height requirement, but are old enough to ride." << endl;
+    }
+
+    return 0;
 }
